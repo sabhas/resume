57ab21d4453876d6517c9dd1ac1d2357b3c09645
Sabir Hassan <sabirbk06@gmail.com>
2023-04-11 01:17:55 +0500

    add links for group chat and resume websites

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,11 +1,13 @@
 const data: { [key: string]: string } = {
   Portfolio: 'https://portfolio.sabhas.me',
   Blog: 'https://blog.sabhas.me',
+  Resume: 'https://resume.sabhas.me',
   Github: 'https://github.com/sabhas',
   Linkedin: 'http://www.linkedin.com/in/sabir-hassan-8b1484140/',
   StackOverflow: 'https://stackoverflow.com/users/12763092/sabir-hassan',
   'Wordle Game': 'https://wordle.sabhas.me',
-  'P2P Chat': 'https://peer-chat.sabhas.me'
+  'P2P Chat': 'https://peer-chat.sabhas.me',
+  'Group Chat': 'https://group-chat.sabhas.me'
 }
 
 export const Links = () => {
