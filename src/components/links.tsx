0e31293ad1da72cb4fc3a30c38f01531c620f4ca
Sabir Hassan <sabirbk06@gmail.com>
2024-02-22 00:15:00 +0500

    fix: update links

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,11 +1,11 @@
 const data: { [key: string]: string } = {
-  Portfolio: 'https://portfolio.sabhas.me',
-  Blog: 'https://blog.sabhas.me',
-  Resume: 'https://resume.sabhas.me',
+  Portfolio: 'https://sabhas.github.io/portfolio/',
+  Blog: 'https://sabhas.github.io/gatsby-blog/',
+  Resume: 'https://sabhas.github.io/resume/',
   Github: 'https://github.com/sabhas',
   Linkedin: 'http://www.linkedin.com/in/sabir-hassan-8b1484140/',
   StackOverflow: 'https://stackoverflow.com/users/12763092/sabir-hassan',
-  'Wordle Game': 'https://wordle.sabhas.me'
+  'Wordle Game': 'https://sabhas.github.io/Wordle/'
   // 'P2P Chat': 'https://peer-chat.sabhas.me',
   // 'Group Chat': 'https://group-chat.sabhas.me'
 }
