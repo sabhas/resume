9c772b4a11e3e4da039a69d1aece642cf98b5d7e
Sabir Hassan <sabirbk06@gmail.com>
2024-05-12 12:59:51 +0500

    add prettier config

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -14,25 +14,23 @@ const skills = [
   'HTML',
   'CSS',
   'Git',
-  'Github Actions'
-]
+  'Github Actions',
+];
 
 export const Skills = () => {
-  const skillSetLength = skills.length
+  const skillSetLength = skills.length;
 
   return (
-    <div className='skills-section'>
-      <div className='section-title'>SKILLS</div>
-      <div className='section-content'>
+    <div className="skills-section">
+      <div className="section-title">SKILLS</div>
+      <div className="section-content">
         {skills.map((skill, index) => (
-          <div className='item'>
+          <div className="item">
             {skill}
-            { index + 1 !== skillSetLength &&
-              <span>,</span>
-            }
+            {index + 1 !== skillSetLength && <span>,</span>}
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
