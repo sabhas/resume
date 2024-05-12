8676a21479431c418e7351cdd6d7c3f2699d25a4
Sabir Hassan <sabirbk06@gmail.com>
2024-05-12 12:36:32 +0500

    fix skills component

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,28 +1,36 @@
-const data = [
-  'Typescript,',
-  'Javascript,',
-  'Node.js,',
-  'Express.js,',
-  'MongoDB,',
-  'Postgresql,',
-  'Type ORM,',
-  'React.js,',
-  'Redux,',
-  'Material UI,',
-  'Bootstrap,',
-  'HTML,',
-  'CSS,',
-  'Git,',
+const skills = [
+  'Typescript',
+  'Javascript',
+  'Node.js',
+  'Express.js',
+  'MongoDB',
+  'Postgresql',
+  'Typeorm',
+  'Nostr',
+  'React.js',
+  'Redux',
+  'Material UI',
+  'Bootstrap',
+  'HTML',
+  'CSS',
+  'Git',
   'Github Actions'
 ]
 
 export const Skills = () => {
+  const skillSetLength = skills.length
+
   return (
     <div className='skills-section'>
       <div className='section-title'>SKILLS</div>
       <div className='section-content'>
-        {data.map((skill) => (
-          <div className='item'>{skill}</div>
+        {skills.map((skill, index) => (
+          <div className='item'>
+            {skill}
+            { index + 1 !== skillSetLength &&
+              <span>,</span>
+            }
+          </div>
         ))}
       </div>
     </div>
