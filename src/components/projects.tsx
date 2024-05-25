127d71d0daee47b5bf0bb206b365ffd5e67012be
Sabir Hassan <sabirbk06@gmail.com>
2024-05-25 10:29:23 +0500

    improve grammer

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,8 +6,8 @@ const data = [
     description:
       'NQuiz is a gamified quiz platform that rewards content creators with Bitcoin (zaps).',
     responsibilities: [
-      'Technologies Used: Typescript, Node.js, Express.js, Nostr, Postgresql, Typeorm and React.js',
-      'My responsibility is to design DB, implement API and implement UI with React.js',
+      'Technologies Used: Typescript, Node.js, Express.js, Nostr, Postgresql, Typeorm, and React.js',
+      'My responsibility is to design DB, implement API, and implement UI with React.js',
     ],
   },
   {
@@ -15,22 +15,22 @@ const data = [
     title: 'SASJS Server',
     link: 'https://github.com/sasjs/server',
     description:
-      'It provides a NodeJS wrapper for calling the SAS, Python, Node.js and R binary executable',
+      'It provides a NodeJS wrapper for calling the SAS, Python, Node.js, and R binary executable',
     responsibilities: [
-      'Technologies Used: Typescript, Node.js, Express.js, MongoDB and React.js',
-      'It has virtual filesystem for storing programs and other content, also the ability to execute Stored Programs from a URL and has REST API with Swagger Docs',
+      'Technologies Used: Typescript, Node.js, Express.js, MongoDB, and React.js',
+      'It has a virtual filesystem for storing programs and other content, also the ability to execute Stored Programs from a URL and has REST API with Swagger Docs',
       'The web component incorporates a Monaco editor for program editing, creation, and saving on a virtual filesystem',
-      'My responsibility is to add new requested features, write unit tests for api endpoints and fix bugs reported by users',
+      'My responsibility is to add new requested features, write unit tests for API endpoints, and fix bugs reported by users',
     ],
   },
   {
     duration: 'May 2021 - Present',
     title: 'SASJS VSCode-Extension',
     link: 'https://github.com/sasjs/vscode-extension',
-    description: `It's a vscode extension written in Typescript for <a href="https://github.com/sasjs" target="_blank">SASJS</a> based projects and files containing .sas extension.`,
+    description: `It's a vs code extension written in Typescript for <a href="https://github.com/sasjs" target="_blank">SASJS</a>-based projects and files containing .sas extension.`,
     responsibilities: [
       'Generate HTML docs from SAS code; Run SAS code; Lint and Format SAS code',
-      '"syncDirectories" feature lets you map one or more LOCAL (on your laptop) directories with one or more REMOTE (on the SAS server) directories.',
+      'The "syncDirectories" feature lets you map one or more LOCAL (on your laptop) directories with one or more REMOTE (on the SAS server) directories.',
       'Compile build and deploy SAS programs to remote servers',
       'My responsibility is to add new requested features and fix bugs reported by users.',
     ],
@@ -42,7 +42,7 @@ const data = [
     description:
       'It is a Command-Line Interface to assist with creating, building, and deploying Data Science projects and HTML5 Web Applications on the SAS platform.',
     responsibilities: [
-      'My responsibility is to add new features, write unit tests and fix bugs reported by users.',
+      'My responsibility is to add new features, write unit tests, and fix bugs reported by users.',
     ],
   },
   {
@@ -52,7 +52,7 @@ const data = [
     description:
       'An adapter for bidirectional SAS® <-> Javascript communication',
     responsibilities: [
-      'My responsibility is to add new features, write unit tests and fix bugs reported by users.',
+      'My responsibility is to add new features, write unit tests, and fix bugs reported by users.',
     ],
   },
 ];
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -4,28 +4,28 @@ const data = [
     title: 'Full Stack Software Engineer, 4gl',
     responsibilities: [
       'Designed and Implemented scalable application programming interfaces.',
-      'Paired with fellow engineers to brainstorm ideas, debug problems and review code.',
-      'Adopted best coding practices and delivered highest quality scalable solutions.',
+      'Paired with fellow engineers to brainstorm ideas, debug problems, and review code.',
+      'Adopted best coding practices and delivered the highest quality scalable solutions.',
       'Worked with customers to define requirements and troubleshoot issues.',
-      'Contributed in the development and maintenance of <a href="https://github.com/sasjs" target="_blank">SASJS</a> framework.',
-      'Tested, fixed and refactored existing features to upheld coding standards.'
-    ]
-  }
-]
+      'Contributed in the development and maintenance of the <a href="https://github.com/sasjs" target="_blank">SASJS</a> framework.',
+      'Tested, fixed, and refactored existing features to uphold coding standards.',
+    ],
+  },
+];
 
 export const Work = () => {
   return (
-    <div className='work-section'>
-      <div className='section-title'>EMPLOYMENT HISTORY</div>
+    <div className="work-section">
+      <div className="section-title">EMPLOYMENT HISTORY</div>
       {data.map((item) => (
-        <div className='work' key={item.title}>
-          <div className='work-duration'>{item.duration}</div>
-          <div className='work-content'>
-            <div className='work-title'>{item.title}</div>
-            <ul className='work-responsibilities'>
+        <div className="work" key={item.title}>
+          <div className="work-duration">{item.duration}</div>
+          <div className="work-content">
+            <div className="work-title">{item.title}</div>
+            <ul className="work-responsibilities">
               {item.responsibilities.map((responsibility, index) => (
                 <li
-                  className='responsibility'
+                  className="responsibility"
                   key={index}
                   dangerouslySetInnerHTML={{ __html: responsibility }}
                 />
@@ -35,5 +35,5 @@ export const Work = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
