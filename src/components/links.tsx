42ed2c5b6ecf9216f3b9cdd1a910333c34125e64
Sabir Hassan <sabirbk06@gmail.com>
2024-07-20 15:43:27 +0500

    styles fixed

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,27 +1,27 @@
 const data: { [key: string]: string } = {
   Portfolio: 'https://sabhas.github.io/portfolio/',
   Blog: 'https://sabhas.github.io/gatsby-blog/',
-  Resume: 'https://sabhas.github.io/resume/',
+  // Resume: 'https://sabhas.github.io/resume/',
   Github: 'https://github.com/sabhas',
   Linkedin: 'http://www.linkedin.com/in/sabir-hassan-8b1484140/',
   StackOverflow: 'https://stackoverflow.com/users/12763092/sabir-hassan',
-  'Wordle Game': 'https://sabhas.github.io/Wordle/'
+  'Wordle Game': 'https://sabhas.github.io/Wordle/',
   // 'P2P Chat': 'https://peer-chat.sabhas.me',
   // 'Group Chat': 'https://group-chat.sabhas.me'
-}
+};
 
 export const Links = () => {
   return (
-    <div className='links-section'>
-      <div className='section-title'>LINKS</div>
-      <div className='section-content'>
+    <div className="links-section">
+      <div className="section-title">LINKS</div>
+      <div className="section-content">
         {Object.keys(data).map((key) => (
           <>
             <a
-              className='link-item'
+              className="link-item"
               href={data[key]}
-              target='_blank'
-              rel='noopener noreferrer'
+              target="_blank"
+              rel="noopener noreferrer"
             >
               {key}
             </a>
@@ -29,5 +29,5 @@ export const Links = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
